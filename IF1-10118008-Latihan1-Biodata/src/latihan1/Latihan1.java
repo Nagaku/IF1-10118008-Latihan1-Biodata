dd9529a3b9400b60f9526d28146f16e1e873ac96
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-09-26 18:29:39 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan1-Biodata/src/latihan1/Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF1
+ * NIM			: 10118008
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan biodata rockstar
+ */
+public class Latihan1 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+		System.out.println("NAMA\t\t: Michael Nagaku Milenn Salim");
+		System.out.println("ALAMAT\t\t: Jalan Dipatu Ukur 94");   
+		System.out.println("TEMPAT LAHIR\t: Indramayu");
+		System.out.println("TANGGALLAHIR\t: 17 Juni 2000"); 
+		System.out.println("------------------");
+		System.out.println("HOBI\t\t: Menggambar");
+		System.out.println("MAKES\t\t: Telor");
+		System.out.println("MIKES\t\t: Cendol");
+	}
+	
+}
